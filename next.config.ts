8efce719eafe61c744711ab2b8a9f449e6f77127
Tamara Kadyear Saber <tamarakadyear@gmail.com>
2025-05-20 19:06:52 +0200

    ci: Adapt the next.config.ts file for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: process.env.GH_PAGES_BASE_PATH,
   output: "export",
-
+  assetPrefix: process.env.GH_PAGES_BASE_PATH,
 };
 
 export default nextConfig;
